50313a3a214314b3873df39ce59221447c4b925e
Burnout Queen <burnout.qu33n@gmail.com>
2022-02-05 17:08:46 -0500

    first commit

---
+++ src/RecipeCreate.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+function RecipeCreate({ createRecipe }) {
+
+  const [name, setName] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [preparation, setPreparation] = useState("");
+
+  function onSubmitHandler(event) {
+    event.preventDefault();
+    createRecipe({name, cuisine, photo, ingredients, preparation});
+    setName("");
+    setCuisine("");
+    setPhoto("");
+    setIngredients("");
+    setPreparation("");
+  };
+
+  const recipeChangeHandler = ({ target }) => {
+    if(target.name === "name") {
+      setName(target.value);
+    }
+    if(target.name === "cuisine") {
+      setCuisine(target.value);
+    }
+    if(target.name === "photo") {
+      setPhoto(target.value);
+    }
+    if(target.name === "ingredients") {
+      setIngredients(target.value);
+    }
+    if(target.name === "preparation") {
+      setPreparation(target.value);
+    }
+  }
+
+  return (
+    <form className="create" onSubmit={onSubmitHandler} name="create">
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <input
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Name"
+                onChange={recipeChangeHandler}
+                value={name}
+              />
+            </td>
+            <td>
+              <input
+                id="cuisine"
+                type="text"
+                name="cuisine"
+                placeholder="Cuisine"
+                onChange={recipeChangeHandler}
+                value={cuisine}
+              />
+            </td>
+            <td>
+              <input
+                id="photo"
+                type="url"
+                name="photo"
+                placeholder="URL"
+                onChange={recipeChangeHandler}
+                value={photo}
+              />
+            </td>
+            <td>
+              <textarea
+                id="ingredients"
+                name="ingredients"
+                placeholder="Ingredients"
+                onChange={recipeChangeHandler}
+                value={ingredients}
+                rows={3} />
+            </td>
+            <td>
+              <textarea
+                id="preparation"
+                name="preparation"
+                placeholder="Preparation"
+                onChange={recipeChangeHandler}
+                value={preparation}
+                rows={3} />
+            </td>
+            <td>
+              <button type="submit">Create</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
+}
+
+export default RecipeCreate;
